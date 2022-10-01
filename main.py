7818983d9a209940d5f03121f8c9a1d451255805
Bluethingi <Olgasthomas@hotmail.com>
2022-10-01 11:03:18 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,31 @@
+
+import xml.sax
+import numpy as np
+import xml.etree.ElementTree as et
+
+class MatrixHandler():
+    def __init__(self):
+        self.CurrentData = ""
+        self.matrix = np.zeros(shape=(4,4))
+        self.xmlTree = et.parse('transformationMatrix.xml')
+
+    def __str__(self):
+        print(f"Maxtrix representation: \n {self.matrix}")
+
+
+    def Element(self):
+        for matrix in self.xmlTree.iter('matrix'):
+            for rows in matrix.iter('row'):
+                for elements in rows.iter('element'):
+                    row = int(rows.attrib['num'])
+                    col = int(elements.attrib['col'])
+                    self.matrix[row-1, col-1] = float(elements.text)
+
+    def multiply(self):
+
+
+
+if (__name__ == "__main__"):
+    Handler = MatrixHandler()
+    Handler.Element()
+    Handler.__str__()
